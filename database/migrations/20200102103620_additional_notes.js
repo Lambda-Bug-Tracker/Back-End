b533a952846de9d01abe349577ad6f695350e144
Brianna Keune <briannakeune@gmail.com>
2020-01-02 10:46:39 -0600

    additional_notes migration config

--- database/migrations/20200102103620_additional_notes.js
+++ database/migrations/20200102103620_additional_notes.js
@@ -1,8 +1,10 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("additional_notes", tbl => {
+    tbl.increments();
+    tbl.text("note").notNullable;
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("additional_notes");
 };
