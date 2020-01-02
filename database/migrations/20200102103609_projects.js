f037a1b5ec72c81dc159c9d4f0944323fc504d3b
Brianna Keune <briannakeune@gmail.com>
2020-01-02 10:40:40 -0600

    projects migration config

--- database/migrations/20200102103609_projects.js
+++ database/migrations/20200102103609_projects.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("projects", tbl => {
+    tbl.increments();
+    tbl
+      .string("name", 128)
+      .unique()
+      .notNullable();
+    tbl
+      .text("description")
+      .unique()
+      .notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("projects");
 };
