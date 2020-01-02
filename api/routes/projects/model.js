f0f83d0f755e74f2b5de1b6a92a1a45ea7185e45
Brianna Keune <briannakeune@gmail.com>
2020-01-02 16:08:11 -0600

    addProject model

--- api/routes/projects/model.js
+++ api/routes/projects/model.js
@@ -1,14 +1,26 @@
 const db = require("../../../database/dbConfig.js");
 module.exports = {
-  getByUserId
+  getByUserId,
+  addProject
 };
 
 function getByUserId(userID) {
-  console.log("userId: ", userID);
-
   return db("user_projects as up")
     .join("users", "users.id", "=", "up.user_id")
-    .join("projects", "projects.id", "=", "up.project_id", )
+    .join("projects", "projects.id", "=", "up.project_id")
     .select("projects.name as project_name", "projects.description")
     .where("up.user_id", userID);
+}
+
+async function addProject(project, userID) {
+  const id = await db("projects").insert(project);
+
+  const newUserProject = {
+    user_id: userID,
+    project_id: id[0]
+  };
+
+   await db("user_projects").insert(newUserProject);
+
+  return getByUserId(userID);
 }
--- api/routes/projects/route.js
+++ api/routes/projects/route.js
@@ -1,16 +1,29 @@
 const express = require("express");
 const router = express.Router();
-const { getByUserId } = require("./model.js");
+const { getByUserId, addProject } = require("./model.js");
 
-router.get("/:id", async(req, res) => {
+router.get("/:id", async (req, res) => {
   console.log("params", req.params);
   const { id } = req.params;
   console.log("get.id", id);
-//   const projects = await getByUserId(id)
-//     res.status(200).json(projects)
-    getByUserId(id)
+
+  getByUserId(id)
     .then(projects => res.status(200).json({ projects }))
     .catch(err => res.status(500).json({ err: err.message }));
+});
+
+router.post("/:id", (req, res) => {
+  const { id } = req.params;
+  const body  = req.body;
+
+  console.log('id, body:', id, body)
+
+  const idIsNum = Number(id)
+  addProject(body, idIsNum)
+    .then(newProject => {
+        console.log(newProject)
+        res.status(201).json({ newProject })})
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
 module.exports = router;
