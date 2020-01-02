7c3223aae0187e777292fc1076f41260a166dcff
Brianna Keune <briannakeune@gmail.com>
2020-01-02 10:43:30 -0600

    bugs migrate config

--- database/migrations/20200102103614_bugs.js
+++ database/migrations/20200102103614_bugs.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("bugs", tbl => {
+    tbl.increments();
+    tbl.string("name", 128).notNullable();
+    tbl.text("description").notNullable();
+    tbl.integer("priority_tag").defaultTo(0);
+    tbl.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("bugs");
 };
