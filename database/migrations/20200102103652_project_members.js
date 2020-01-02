1a77e2e679873d5ccdd5161de2d70a4e6fd7249d
Brianna Keune <briannakeune@gmail.com>
2020-01-02 10:56:35 -0600

    project_members migrate config

--- database/migrations/20200102103652_project_members.js
+++ database/migrations/20200102103652_project_members.js
@@ -1,8 +1,26 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("project_members", tbl => {
+    tbl.increments();
+    tbl.integer("is_project_admin").defaultTo(0);
+    tbl
+      .integer("user_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl
+      .integer("project_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("projects")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("project_members");
 };
