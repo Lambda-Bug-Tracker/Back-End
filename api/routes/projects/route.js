a349b83cce128ef93c16d81cfe8dd5976c650808
Brianna Keune <briannakeune@gmail.com>
2020-01-02 13:24:31 -0600

    added get user project

---
+++ api/routes/projects/route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { getByUserId } = require("./model.js");
+router.get("/:user_id", (req, res) => {
+  const id = req.params.user_id;
+  getByUserId(id)
+    .then(projects => res.status(200).json({ projects }))
+    .catch(err => res.status(500).json({ failed: 'this failed 💩' }));
+});
