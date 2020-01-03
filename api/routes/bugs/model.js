1d2457ed6d127692f8c66fc158ccb4bf9222e8c6
JimmyMcBride <mcbride967@gmail.com>
2020-01-02 23:36:03 -0600

    added post request for bugs

--- api/routes/bugs/model.js
+++ api/routes/bugs/model.js
@@ -1,17 +1,29 @@
-const db = require('../../../database/dbConfig');
+const db = require("../../../database/dbConfig");
 
 module.exports = {
-    getByProjectId
-}
+  getByProjectId,
+  createBug,
+  createProjectBug
+};
 
 function getByProjectId(projectID) {
-    console.log("projectID: ", projectID)
-    return db("project_bugs as pb")
-        .join("bugs", "bugs.id", "=", "pb.bug_id")
-        .join("projects", "projects.id", "=", "pb.project_id")
-        // .select("bugs.name as bugs_name", "bugs.description as bugs_description",
-        //     "bugs.priority_tag as priority_tag", "bugs.created_at as created_at"
-        // )
-        .select("bugs.name as bug_name")
-        .where("pb.project_id", projectID)
+  console.log("projectID: ", projectID);
+  return (
+    db("project_bugs as pb")
+      .join("bugs", "bugs.id", "=", "pb.bug_id")
+      .join("projects", "projects.id", "=", "pb.project_id")
+      // .select("bugs.name as bugs_name", "bugs.description as bugs_description",
+      //     "bugs.priority_tag as priority_tag", "bugs.created_at as created_at"
+      // )
+      .select("bugs.name as bug_name")
+      .where("pb.project_id", projectID)
+  );
+}
+
+function createBug(bug) {
+  return db("bugs").insert(bug);
+}
+
+function createProjectBug(IDs) {
+  return db("project_bugs").insert(IDs);
 }
--- api/routes/bugs/route.js
+++ api/routes/bugs/route.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const { getByProjectId } = require('./model.js');
+const { getByProjectId, createBug, createProjectBug } = require("./model.js");
 
-router.get("/project/:id", (req, res) => {
-    const { id } = req.params;
+/* @api */
 
-    getByProjectId(id)
-        .then(bugs => res.status(200).json({ bugs }))
-        .catch(err => res.status(500).json({ err: err.message }))
-})
+router.get("/:project_id", (req, res) => {
+  const { project_id } = req.params;
+
+  getByProjectId(project_id)
+    .then(bugs => res.status(200).json({ bugs }))
+    .catch(err => res.status(500).json({ err: err.message }));
+});
+
+router.post("/:project_id", async (req, res) => {
+  const { project_id } = req.params;
+  try {
+    if (req.body) {
+      const bug = await createBug(req.body);
+      const IDs = {
+        bug_id: bug[0],
+        project_id: project_id
+      };
+      await createProjectBug(IDs);
+      res.status(201).json({ message: "Success 🍻" });
+    } else {
+      res.status(400).json({ message: "Need to send bug in body of request." });
+    }
+  } catch (err) {
+    res.status(500).json({ errMessage: err.message, devMessage: "💩" });
+  }
+});
 
 module.exports = router;
