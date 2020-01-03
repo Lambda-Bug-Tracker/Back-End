2c128c78e08dd881054bfacc6c7b980c229374df
Brianna Keune <briannakeune@gmail.com>
2020-01-02 20:46:40 -0600

    users migration

--- database/migrations/20200102204326_users.js
+++ database/migrations/20200102204326_users.js
@@ -1,8 +1,21 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments();
+    tbl
+      .integer("firebase_id")
+      .unique()
+      .notNullable();
+    tbl
+      .string("email", 128)
+      .unique()
+      .notNullable();
+    tbl
+      .string("display_name", 128)
+      .unique()
+      .notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTableIfExists('users')
 };
