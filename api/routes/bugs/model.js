819ec3176a4fef886d584dc8325b29a9c4f45b8c
JimmyMcBride <mcbride967@gmail.com>
2020-01-03 10:42:47 -0600

    added delete request and get bug by bug_id

--- api/routes/bugs/model.js
+++ api/routes/bugs/model.js
@@ -2,10 +2,14 @@ const db = require("../../../database/dbConfig");
 
 module.exports = {
   getByProjectId,
+  getByBugId,
+  findProjectBug,
   createBug,
   createProjectBug,
   findBug,
-  updateBug
+  updateBug,
+  removeBug,
+  removeProjectBug
 };
 
 function getByProjectId(projectID) {
@@ -30,6 +34,28 @@ function getByProjectId(projectID) {
   );
 }
 
+function getByBugId(bug_id) {
+  console.log("bug_id: ", bug_id);
+  return (
+    db("project_bugs as pb")
+      .join("bugs", "bugs.id", "=", "pb.bug_id")
+      .join("projects", "projects.id", "=", "pb.project_id")
+      // .select("bugs.name as bugs_name", "bugs.description as bugs_description",
+      //     "bugs.priority_tag as priority_tag", "bugs.created_at as created_at"
+      // )
+      .select(
+        "bugs.id",
+        "bugs.name as bug_name",
+        "bugs.description",
+        "bugs.priority_tag",
+        "bugs.progress_tag",
+        "bugs.hash_tag",
+        "bugs.created_at"
+      )
+      .where("pb.bug_id", bug_id)
+  );
+}
+
 function createBug(bug) {
   return db("bugs").insert(bug);
 }
@@ -44,9 +70,28 @@ function findBug(id) {
     .first();
 }
 
+function findProjectBug(bug_id) {
+  return db("project_bugs")
+    .join("bugs", "bugs.id", "=", "project_bugs.bug_id")
+    .select("project_bugs.id", "project_bugs.bug_id")
+    .where("project_bugs.bug_id", bug_id);
+}
+
 async function updateBug(bug, id) {
   await db("bugs")
     .where({ id })
     .update(bug);
   return findBug(id);
+}
+
+function removeBug(id) {
+  return db("bugs")
+    .where({ id: id })
+    .del();
+}
+
+function removeProjectBug(bug_id) {
+  return db("project_bugs")
+    .where({ bug_id: bug_id })
+    .del();
 }
